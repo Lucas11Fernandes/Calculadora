31ed73a503da24f91738cbfca2dd5fe88f8a0699
Lucas <lukas741852@hotmail.com>
2023-04-27 15:24:35 -0300

    botando a calculadora pronta

--- calc.js
+++ calc.js
@@ -1,48 +1,101 @@
+// Variável global que armazena os valores a serem calculados
+var vdisplay = [];
 
-// declaração da variável global document denominada como d
-let d = document;
+// Variável global que armazena o objeto 'document' do navegador
+var d = document;
 
-function insert(num)
-{
-    
-	//Pegar o valor do objeto HTML resultado e armazena na variável número
-	var numero = d.getElementById('resultado').innerHTML;
-	//Faz a concatenação da variável com o valor numérico indicado pela tecla
-    d.getElementById('resultado').innerHTML = numero + num;
-	
-	
-	//Forma avançada de representar o mesmo algoritmo do bloco acima
-	//d.getElementById('resultado').innerHTML += num;
-	
-}
-
-function clean()
-{
-    
-	// Limpa todos os valores contidos no objeto Html resultado
+// Função para limpar o campo de resultado e o array de valores a serem calculados
+function clean() {
+	// Limpa o campo de resultado
 	d.getElementById('resultado').innerHTML = "";
+	// Limpa o array de valores
+	vdisplay = [];
 }
-function back()
-{
-    // Armazena o conteúdo do objeto Html resultado na variável denominada como resultado
-	var resultado = d.getElementById('resultado').innerHTML;
-	// Usa a função substring para transformar a variável resultado em um vetor e subtrair uma posição do vetor, retornando o resultado para o objeto Html resultado
-    d.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+// Função para apagar o último caractere digitado no campo de resultado
+function back() {
+	// Obtém o valor atual do campo de resultado
+	let texto = d.getElementById('resultado').innerHTML;
+	// Remove o último caractere do valor obtido
+	d.getElementById('resultado').innerHTML = texto.substring(0, texto.length-1);
 }
-function calcular()
-{
-    // Armazena o conteúdo do objeto Html resultado na variável denominada como resultado
-	var resultado = d.getElementById('resultado').innerHTML;
-    
-	// Verifica se existe um valor na varíável
-	if(resultado)
-    {
-        // Se existe um valor é executado a função eval para calcular qualquer valor que está na variável resultado e retornado o resultado para o objeto Html resultado
-		d.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    // Caso o objeto Html resultado esteja vazio é retornado um valor "Nada..."
-	else
-    {
-		d.getElementById('resultado').innerHTML = "Nada..."
-    }
+
+// Função para adicionar um valor ou operador ao campo de resultado
+function insert(num) {
+	// Verifica se o valor passado é um operador (+, -, /, *) 
+	if (num == "+" || num == "-" || num == "/" || num == "*") {
+		// Verifica se o array de valores está vazio
+		if (vdisplay.length == 0) {
+			// Adiciona o valor atual do campo de resultado ao array de valores
+			vdisplay.push(d.getElementById('resultado').innerHTML);
+			// Adiciona o operador ao array de valores
+			vdisplay.push(num);
+			// Limpa o campo de resultado
+			d.getElementById('resultado').innerHTML = "";
+		} else if (vdisplay[vdisplay.length-1] != d.getElementById('resultado').innerHTML && d.getElementById('resultado').innerHTML != "") {
+			// Adiciona o valor atual do campo de resultado ao array de valores
+			vdisplay.push(d.getElementById('resultado').innerHTML);
+			// Adiciona o operador ao array de valores
+			vdisplay.push(num);
+			// Limpa o campo de resultado
+			d.getElementById('resultado').innerHTML = "";
+		} else if (vdisplay[vdisplay.length-1] != "+" && vdisplay[vdisplay.length-1] !="-" && vdisplay[vdisplay.length-1] != "/" && vdisplay[vdisplay.length-1] != "*") {
+			// Adiciona o operador ao array de valores
+			vdisplay.push(num);
+			// Limpa o campo de resultado
+			d.getElementById('resultado').innerHTML = "";
+		}
+	} else if (num =="=") {
+		// Verifica se o último valor do array de valores é um operador
+		if (vdisplay[vdisplay.length-1] == "+" || vdisplay[vdisplay.length-1] =="-" || vdisplay[vdisplay.length-1] =="/" || vdisplay[vdisplay.length-1] == "*") {
+			// Adiciona o valor atual do campo de resultado ao array de valores
+			vdisplay.push(d.getElementById('resultado').innerHTML);
+			// Chama a função para calcular o resultado
+			calcular();
+		}
+	}else{
+		// Verifica se o array de valores contém somente um valor
+		if (vdisplay.length == 1) {
+			// Chama a função para limpar o campo de resultado e o array de valores
+			clean();
+			// Adiciona o valor ao campo de resultado
+			d.getElementById('resultado').innerHTML += num;
+		}else{
+            // Adiciona o valor ao campo de resultado
+			d.getElementById('resultado').innerHTML += num;
+		}
+	}
+}
+
+// Cria uma função chamada "calcular"
+function calcular(){ 
+	// Declara uma variável "result" vazia
+	let result = "";
+
+	// Inicia um bloco "try" para lidar com exceções
+	try{ 
+		// Inicia um laço de repetição "for" com uma variável "i" inicializada como 0 e um limite de repetições igual ao tamanho do array "vdisplay"
+		for(let i = 0 ; i <vdisplay.length; i++) { 
+			// Adiciona o valor da posição atual do array "vdisplay" à variável "result"
+			result += vdisplay[i]; 
+			// Exibe o valor da variável "result" no console com a mensagem "Resultado ="
+			console.log(" Resultado = " + result); 
+
+		}
+		// Executa a função "eval" com o valor da variável "result" como argumento e armazena o resultado de volta na variável "result"
+		result = eval(result); 
+		// Define o valor da propriedade "innerHTML" do elemento HTML com o id "resultado" como o valor da variável "result"
+		d.getElementById('resultado').innerHTML = result; 
+		// Define o array "vdisplay" como vazio
+		vdisplay = []; 
+		// Adiciona o valor da variável "result" ao final do array "vdisplay"
+		vdisplay.push(result); 
+		// Exibe o valor do array "vdisplay" no console
+		console.log(vdisplay); 
+	// Se ocorrer uma exceção durante a execução do bloco "try", entra no bloco "catch"
+	}catch(e){ 
+		// Define o valor da propriedade "innerHTML" do elemento HTML com o id "resultado" como "--- Nan ---"
+		d.getElementById('resultado').innerHTML = " --- Nan ---"; 
+	}
+
 }
